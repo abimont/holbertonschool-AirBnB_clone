ac329f7b320b925c1f11b229c3fc7708cc4f3d3a
MiguelGit20 <miguel.torres.20@hotmail.com>
2022-10-27 11:19:26 -0500

    Unittest: documentaion

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -13,25 +13,47 @@ class TestBaseModel(unittest.TestCase):
     """
 
     def setUp(self):
+        """
+        Setting up the instances
+        """
+
         self.instanceOne = BaseModel()
         self.instanceTwo = BaseModel()
 
     def test_BaseModel(self):
+        """
+        Testing:
+            - save() method
+            - updated_at attribute
+        """
+
         firstUpdate = self.instanceOne.updated_at
         self.instanceOne.save()
         secondUpdate = self.instanceOne.updated_at
         self.assertNotEqual(firstUpdate, secondUpdate)
 
     def test_id(self):
+        """
+        Testing if every instances's id is unique
+        """
+
         self.assertNotEqual(self.instanceOne.id, self.instanceTwo.id)
 
     def test_dict(self):
+        """
+        Testing to_dict() method
+        """
+
         self.assertIn("id", self.instanceOne.to_dict())
         self.assertIn("__class__", self.instanceOne.to_dict())
         self.assertIn("created_at", self.instanceOne.to_dict())
         self.assertIn("updated_at", self.instanceOne.to_dict())
 
     def test_str(self):
+        """
+        Testing __str__() method
+        """
+
         self.assertIsInstance(self.instanceOne.__str__(), str)
         self.assertIn(self.instanceOne.id, self.instanceOne.__str__())
         self.assertIn(type(self.instanceOne).__name__,
