de2cc15e9cc15347f69e6004a375d40bcdb1115f
MiguelGit20 <miguel.torres.20@hotmail.com>
2022-10-28 20:40:24 -0500

    Update: to_ dict method

--- models/base_model.py
+++ models/base_model.py
@@ -60,7 +60,7 @@ class BaseModel:
         """
 
         inst_dict = self.__dict__.copy()
-        inst_dict['__class__'] = type(self).__name__
         inst_dict['created_at'] = self.created_at.isoformat()
         inst_dict['updated_at'] = self.updated_at.isoformat()
+        inst_dict['__class__'] = self.__class__.__name__
         return inst_dict
