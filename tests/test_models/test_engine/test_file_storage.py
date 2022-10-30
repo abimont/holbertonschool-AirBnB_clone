4a1a8a76aff0b962c01da8c76a0ddeed5a4db0b6
abimont <abimontes00@gmail.com>
2022-10-29 22:30:30 -0500

    Update: provisional

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -6,7 +6,6 @@ Tests for FileStorage class
 import unittest
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
-from models import storage
 
 
 class TestFileStorage(unittest.TestCase):
@@ -14,30 +13,10 @@ class TestFileStorage(unittest.TestCase):
     Tests FileStorage class methods
     """
 
-    def test_file_storage(self):
+    def test_file_storage_attributes(self):
         """
-        Testing FileStorage methods
+        Testing FileStorage atributtes
         """
-        dicOne = FileStorage.all(self)
-        self.assertIsInstance(dicOne, dict)
-        self.assertEqual(len(dicOne), 0)
-        instanceBase = BaseModel()
-        self.assertGreater(len(dicOne), 0)
-
-    def test_save(self):
-        """
-        Provisional test
-        """
-        instanceBM = BaseModel()
-        instanceFS = FileStorage()
-        self.assertEqual(instanceBM.save(), None)
-        self.assertEqual(instanceFS.reload(), None)
-
-    def test_object(self):
-        """
-        Testing FileStorage attributes
-        """
-        instance = FileStorage()
-        with self.assertRaises(AttributeError):
-            instance.__objects
-            instance.__file_path
+        storage = FileStorage()
+        self.assertEqual(storage._FileStorage__file_path, 'file.json')
+        self.assertDictEqual(storage._FileStorage__objects, {})
