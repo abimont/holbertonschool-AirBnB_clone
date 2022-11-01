7e07b7656b35a37c7014394f4298e2070b62836c
abimont <abimontes00@gmail.com>
2022-11-01 11:52:07 -0500

    Unittest: test for City class

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module ``test_city``
+Tests for City class
+"""
+
+
+import unittest
+from models.city import City
+
+
+class TestState(unittest.TestCase):
+    """
+    Test class for City
+    """
+
+    def test_city(self):
+        """
+        Testing City attributes
+        """
+
+        instance = City()
+        self.assertEqual(instance.state_id, "")
+        self.assertEqual(instance.name, "")
